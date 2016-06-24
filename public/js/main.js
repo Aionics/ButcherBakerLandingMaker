594d972e795dcf017177d6503563df7079322dee
Kosarev Artem <Aionics@list.ru>
2016-06-24 23:15:09 +0300

    creating customers fn added

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,26 @@
-ko.applyBindings({
-    customers: [
-            { name: 'Вася'},
-            { name: 'Петя'},
-            { name: 'Борис'}
-    ]
-});
+function Customer(name, image){
+    this.name = name;
+    this.image = image;
+    this.consist = 'new';
+}
+
+function viewModel() {
+    var self = this;
+    self.customers = ko.observableArray();
+
+    self.refreshArray = function(){
+        var data = self.customers().slice(0);
+        self.customers([]);
+        self.customers(data);
+    };
+    self.createNewCustomer = function() {
+        console.log(1);
+        var newCustomer = new Customer('Vasya', 1);
+        self.customers.push(newCustomer);
+    }
+    self.changeCustomerConsist = function() {
+        this.consist = 'in-progress';
+        self.refreshArray();
+    }
+};
+ko.applyBindings(new viewModel());
