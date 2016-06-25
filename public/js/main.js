acd0c2d49e4bba19998ac073dc0374c1c3726517
aionics <aionics@list.ru>
2016-06-26 01:16:52 +0500

    deadline bar added, but need to fix it

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 var arrayCustomers = [];
+var varActiveCustomer;
+var test = {};
+test.name = '50%'
 
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,13 +10,13 @@ function Customer(name, image){
     this.name = dictionary.customersNames[randomInt(0, dictionary.customersNames.length-1)];
     this.image = image;
     this.consist = 'new';
-    this.deadline = randomInt(500,1000);
+    this.deadline = randomInt(50,100);
 }
 
 function viewModel() {
     var self = this;
     self.customers = ko.observableArray(arrayCustomers);
-    self.activeCustomer = ko.observableArray();
+    self.activeCustomer = ko.observable(new Customer('Vasya', 1)).extend({notify: 'always'});
 
     self.refreshArray = function(){
         var data = self.customers().slice(0);
@@ -27,16 +30,23 @@ function viewModel() {
     }
     self.selectCustomer = function(){
         this.consist = 'in-progress';
-        self.activeCustomer = this;
+        varActiveCustomer =this;
+        self.activeCustomer(varActiveCustomer);
+        console.log(this.deadline);
         self.refreshArray();
     }
 };
-ko.applyBindings(new viewModel());
+var vm = new viewModel();
+ko.applyBindings(vm);
 
 function deadlineCounter() {
     arrayCustomers.forEach(function(value, key, arr) {
         if (value.deadline > 0 && value.consist == 'in-progress')
-            arr[key].deadline = value.deadline - 1;
+            arr[key].deadline = value.deadline - 0.1;
+            if(value === vm.activeCustomer()){
+                vm.activeCustomer(varActiveCustomer).deadline = value.deadline;
+            }
     });
 }
-setInterval( deadlineCounter , 100)
+console.log('timer');
+setInterval(deadlineCounter, 100)
